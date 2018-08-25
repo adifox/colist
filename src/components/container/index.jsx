14b81776076de37b1047dade9672b4b700a64208
Lukas <lukasdevarga@gmail.com>
2018-08-25 12:38:34 +0200

    basic routing added

--- src/components/container/index.jsx
+++ src/components/container/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
 
 import Profile from '../profile';
 import HomeContainer from '../homeContainer';
@@ -6,10 +7,10 @@ import HomeContainer from '../homeContainer';
 class container extends Component {
   render () {
     return(
-      <div>
-        <HomeContainer />
-        <Profile />
-      </div>
+        <Switch>
+          <Route exact path='/' component={ HomeContainer }/>
+          <Route exact path='/profile' component={ Profile }/>
+        </Switch>
     )
   }
 }
